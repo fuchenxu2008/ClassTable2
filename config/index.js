d2ffb780737c6d1740544e2d9e13e33ae96db50c
fuchenxu2008 <fuchenxu2008@163.com>
2019-04-05 17:30:23 +0800

    db auth

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const dbConfig = require('./dbConfig');
 
 const config = {
-    mongoURL: process.env.MONGO_URL || `mongodb://${dbConfig.uname}:${dbConfig.psw}@${dbConfig.url}/${dbConfig.database}`,
+    mongoURL: process.env.MONGO_URL || `mongodb://${dbConfig.uname}:${dbConfig.psw}@${dbConfig.url}/${dbConfig.database}?authSource=admin`,
     port: process.env.PORT || 8080,
     secret: 'xjtlu215123'
 }
